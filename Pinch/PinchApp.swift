4b821cf1e63fd55a073bd0a3962b7f4fdc6d1fc3
chitraarasu <kchitraarasu>
2023-02-13 12:02:22 +0530

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by kirshi on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
